ae86cacc0ab138e05701d394af7c9a13d52ba859
kimx <gn01124620@gmail.com>
2015-07-08 10:50:23 +0800

    first

---
+++ ProtractorAdvanceLab/protractor-conf.js
@@ -0,0 +1,28 @@
+﻿exports.config = {
+    allScriptsTimeout: 11000,
+    framework: 'jasmine2',
+    jasmineNodeOpts: {
+        defaultTimeoutInterval: 30000,
+        showColors: true,
+    },
+    capabilities: { 'browserName': 'chrome' },
+    rootElement: '[ng-app]',
+    specs: [
+    'spec/*.js'
+    ],
+    params: {
+        login: { userName: 'developer', password: 'mis123' }
+        , baseUrl: "http://localhost:2016/"
+    },
+
+    onPrepare: function () {
+        var jasmineReporters = require('jasmine-reporters');
+        jasmine.getEnv().addReporter(new jasmineReporters.JUnitXmlReporter({
+            consolidate: false,
+            consolidateAll: false,
+            savePath: 'test-results',
+            filePrefix: "e2e-",
+        }));
+    }
+
+};
---
+++ ProtractorAdvanceLab/spec/caculator-spec.ts
@@ -0,0 +1,15 @@
+﻿describe('Protractor Demo App', function () {
+    it('should have a title', function () {
+        browser.get('http://juliemr.github.io/protractor-demo/');
+        expect(browser.getTitle()).toEqual('Super Calculator');
+    });
+
+    it('should add one and two', function () {
+        browser.get('http://juliemr.github.io/protractor-demo/');
+        element(by.model('first')).sendKeys("1");
+        element(by.model('second')).sendKeys("2");
+        element(by.id('gobutton')).click();
+        browser.sleep(10000);
+        expect(element(by.binding('latest')).getText()).toEqual('3');
+    });
+});
