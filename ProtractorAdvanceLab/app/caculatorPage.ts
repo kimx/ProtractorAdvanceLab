82f02c0167d49553e52a2ea3f4c59536776d200f
kimx <gn01124620@gmail.com>
2015-07-08 11:41:17 +0800

    page object

---
+++ ProtractorAdvanceLab/app/caculatorPage.ts
@@ -0,0 +1,18 @@
+﻿export class caculatorPage {
+    elFirst = element(by.model('first'));
+    elSecond = element(by.model('second'));
+    btnGo = element(by.id('gobutton'));
+    elLatest = element(by.binding('latest'));
+
+    open() {
+        browser.get('http://juliemr.github.io/protractor-demo/');
+    }
+
+    caculate(first, second) {
+        this.elFirst.sendKeys(first);
+        this.elSecond.sendKeys(second);
+        this.btnGo.click();
+    }
+
+
+}
---
+++ ProtractorAdvanceLab/spec/caculator-spec-page-object.ts
@@ -0,0 +1,15 @@
+﻿import app = require("../app/caculatorPage");
+
+describe('Protractor Demo App by Page Object', function () {
+    var page = new app.caculatorPage();
+    it('should have a title', function () {
+        page.open();
+        expect(browser.getTitle()).toEqual('Super Calculator');
+    });
+
+    it('should add one and two', function () {
+        page.open();
+        page.caculate(1, 2);
+        expect(page.elLatest.getText()).toEqual('3');
+    });
+});
